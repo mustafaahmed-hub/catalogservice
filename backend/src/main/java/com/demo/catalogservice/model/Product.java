ed0b1ad52d2f533ec2cf27b96cc718c17f726c44
saketh reddy <sakethreddynoti@gmail.com>
2021-11-08 23:55:57 +0530

    schema and model modified

--- backend/src/main/java/com/demo/catalogservice/model/Product.java
+++ backend/src/main/java/com/demo/catalogservice/model/Product.java
@@ -1,11 +1,30 @@
 package com.demo.catalogservice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.cassandra.core.mapping.PrimaryKey;
+
+import javax.persistence.*;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table
 public class Product {
-    String id;
-    String name;
-    double price;
-    int weight;
-//    Dimensions dimensions;
-//    Categories categories;
+    @PrimaryKey
+    private UUID id;
+    private String name;
+    private double price;
+    private String imageUrl;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    private ProductDescription description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    private Category category;
 
 }
--- backend/src/main/java/com/demo/catalogservice/repository/ProductRepository.java
+++ backend/src/main/java/com/demo/catalogservice/repository/ProductRepository.java
@@ -1,4 +1,7 @@
 package com.demo.catalogservice.repository;
 
-public interface ProductRepository  {
+import com.demo.catalogservice.model.Product;
+import org.springframework.data.cassandra.repository.CassandraRepository;
+
+public interface ProductRepository  extends CassandraRepository<Product,String> {
 }
