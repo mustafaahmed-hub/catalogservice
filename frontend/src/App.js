f08c18b59f52ca3dd73bc9eead3008fb46d83527
LokeshShankar <40104851+LokeshShankar@users.noreply.github.com>
2021-11-19 11:42:30 +0530

    adding product details

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router } from "react-router-dom";
 import CreateProduct from "./components/createProduct";
 import EditProduct from "./components/editProduct";
 import NoMatch from "./components/noMatch";
+import ProductDetailView from "./components/productDescription/ProductDesc";
 import ProductList from "./components/ProductList";
 import NavHead from "./header/NavHead";
 function App() {
@@ -15,6 +16,8 @@ function App() {
          <Route exact path="/" component={ProductList}/>
          <Route path="/editproduct" component={EditProduct}/>
          <Route path="/createproduct" component={CreateProduct}/>
+         <Route path="/productdesc" component={ProductDetailView}/>
+         
          <Route component={NoMatch}/>
        </Switch>
      </Router>
